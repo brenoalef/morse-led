bc15b82389a98382ca26a358e4df08969088ba11
brenoalef <b.alefdsa@gmail.com>
2017-09-19 12:06:23 -0300

    Update ledController.py
    Using serial port.

--- ledController.py
+++ ledController.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import RPi.GPIO as GPIO
 import time
+import serial
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
+ser = serial.Serial('/dev/ttyS0')
 
 
 def turn_on():
-    print("LED ON\n")
-    GPIO.output(18, 1)
+    ser.setDTR(True)
 
 
 def turn_off():
-    print("LED OFF")
-    GPIO.output(18, 0)
+    ser.setDTR(False)
 
 
 def display_morse(encoded):
-    for c in encoded:
-        print(c)
-        if (c == '.' or c == ' '):
-            time.sleep(0.1)
-        elif (c == '-'):
+    words = encoded.split("/")
+    for word in words:
+        characters = word.split(" ")
+        for character in characters:
+            for component in character:
+                if (component == '.'):
+                    turn_on()
+                    time.sleep(0.1)
+                    turn_off()
+                elif (component == '-'):
+                    turn_on()
+                    time.sleep(0.3)
+                    turn_off()
+                time.sleep(0.1)
             time.sleep(0.3)
-        elif (c == '/'):
-            time.sleep(0.7)
+        time.sleep(0.7)
+
 
 
 if __name__ == '__main__':
